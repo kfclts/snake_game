cfe8c936214aff24225f1bc73a7a06c22e87f330
kchang <kchang@localhost>
2024-07-28 15:44:43 +0800

    Detect collision with tail

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from food import Food
 from scoreboard import ScoreBoard
 import time
 
+GAME_SPEED = 0.1
+
 screen = Screen()
 # screen_width, screen_height = screen.window_width() // 2, screen.window_height() // 2
 screen.setup(width=600, height=600)
@@ -24,7 +26,7 @@ screen.onkey(snake.right, "Right")
 game_is_on = True
 while game_is_on:
     screen.update()
-    time.sleep(0.2)
+    time.sleep(GAME_SPEED)
     snake.move()
     
     ## Detect collision with food
@@ -37,4 +39,13 @@ while game_is_on:
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
         game_is_on = False
         scoreboard.game_over()
+        
+    # Detect collision with tail
+    for segment in snake.segments:
+        if segment == snake.head:
+            pass
+        elif snake.head.distance(segment) < 10:
+            game_is_on = False
+            scoreboard.game_over()
+        
 screen.exitonclick()
