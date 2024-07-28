5774ec1397d2d057a701018d90bf9e180041b632
kchang <kchang@localhost>
2024-07-28 14:50:42 +0800

    refresh the food

--- food.py
+++ food.py
@@ -9,8 +9,17 @@ class Food(Turtle):
         self.shapesize(stretch_len=0.5, stretch_wid=0.5) # defaul is 20, streth to 0.5, is 10
         self.color("blue")
         self.speed("fastest") ## will not see the location created, but show up at the location goto
-        random_x = random.randint(-280, 280) # to prevent collipse the wall
-        random_y = random.randint(-280, 280)
+        self.refresh()
+        
+    def refresh(self):
+        min_val, max_val = -280, 280
+        step = 20
+
+        min_index = min_val // step
+        max_index = max_val // step
+        
+        random_x = random.randint(min_index, max_index) * step
+        random_y = random.randint(min_index, max_index) * step
         self.goto(random_x, random_y)
         
         
